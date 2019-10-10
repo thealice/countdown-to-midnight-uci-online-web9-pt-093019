1fbed715e30ad445d4aca72a8bac3866607feaeb
Alice Freda <alicefreda@gmail.com>
2019-10-10 01:58:22 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,3 +5,12 @@ def countdown(number)
   end
     return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  until number == 0
+    sleep(1)
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+    return "HAPPY NEW YEAR!"
+end
